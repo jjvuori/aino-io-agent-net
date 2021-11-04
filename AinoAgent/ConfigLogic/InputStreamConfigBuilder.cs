9dce189a435d5f4c204d728630c9eb2bde9ef89c
Matti-Oskari Leppänen <matti-oskari.leppanen@entteri.fi>
2021-11-04 22:38:55 +0200

    schema dirty workaround

--- AinoAgent/ConfigLogic/InputStreamConfigBuilder.cs
+++ AinoAgent/ConfigLogic/InputStreamConfigBuilder.cs
@@ -11,10 +11,10 @@ namespace Aino.Agents.Core.Config
 {
     public class InputStreamConfigBuilder : IAgentConfigBuilder
     {
-        
+
         //Todo: Turn on logging
         //private static readonly Log log = LogFactory.GetLog(InputStreamConfigBuilder.class);
-        private static readonly string LOGGER_SCHEMA = "config" + Path.DirectorySeparatorChar + "Logger.xsd";
+        private static readonly string LOGGER_SCHEMA = @"AinoAgent" + Path.DirectorySeparatorChar + "config" + Path.DirectorySeparatorChar + "Logger.xsd";
         
         private const string CONFIG_ENABLED_ATT_Q = "enabled";
         private const string CONFIG_LOGGER_SERVICE_Q = "ainoLoggerService";
